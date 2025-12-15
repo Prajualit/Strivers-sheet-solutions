02fe1a93c1a83076fd1b34824d676b82bee5ecf4
Prajualit Tickoo <23bec072@nith.ac.in>
2025-12-16 00:19:58 +0530

    streaks

--- src/Streaks.java
+++ src/Streaks.java
@@ -882,6 +882,23 @@ public class Streaks {
         return (int) totalWays;
     }
 
+    public long getDescentPeriods(int[] prices) {
+
+        long result = 1;
+        long consecutiveNumbers = 1;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] == prices[i - 1] - 1){
+                consecutiveNumbers++;
+            } else {
+                consecutiveNumbers = 1;
+            }
+            result += consecutiveNumbers;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
 //
 //        int[] nums = {2, 2, 3, 4}; // 3
